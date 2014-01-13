5316a01d996bc6113824f3801b8483afe332a2fe
Xin Sun <sunxin8086@gmail.com>
2014-01-13 01:39:21 -0800

    Add XcodeCoverage folder

---
+++ XSDemo/XcodeCoverage/envcov.sh
@@ -0,0 +1,15 @@
+#
+#   Copyright 2012 Jonathan M. Reid. See LICENSE.txt
+#   Created by: Jon Reid, http://qualitycoding.org/
+#   Source: https://github.com/jonreid/XcodeCoverage
+#
+
+source env.sh
+
+# Change the report name if you like:
+LCOV_INFO=XSDemo.info
+
+LCOV=${LCOV_PATH}/lcov
+OBJ_DIR=${OBJECT_FILE_DIR_normal}/${NATIVE_ARCH}
+
+# Added modules
